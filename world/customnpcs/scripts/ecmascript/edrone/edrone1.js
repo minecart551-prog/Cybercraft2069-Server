28c98c00675fc7ad41888adc1e2b02e584fe5c99
minecart551-prog <minecart551@gmail.com>
2026-07-10 14:15:49 +0700

    Set NPC respawn type to 4

--- world/customnpcs/scripts/ecmascript/edrone/edrone1.js
+++ world/customnpcs/scripts/ecmascript/edrone/edrone1.js
@@ -3,6 +3,7 @@ function init(event) {
     npc.getAi().setNavigationType(1);
     npc.getStats().setMaxHealth(27);
     npc.getStats().getRanged().setStrength(3);
+    npc.getStats().setRespawnType(4);
     npc.getStats().setRespawnTime(1800);
     npc.getStats().getRanged().setDelay(7, 7);
     npc.getStats().getRanged().setBurstDelay(1);
