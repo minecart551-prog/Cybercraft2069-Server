f93e1562265781efcd17f672cfbe5c6daa8c86ae
minecart551-prog <minecart551@gmail.com>
2026-07-28 08:07:26 +0700

    menu update

--- world/customnpcs/scripts/ecmascript/menu.js
+++ world/customnpcs/scripts/ecmascript/menu.js
@@ -47,6 +47,8 @@ TUTORIAL_TOPICS[TU_TOPICS.BASIC] = {
         "\u00a7a\ud83c\udf5f \u00a7fBuy food from \u00a7evending machines\u00a7f in city",
         "\u00a7b/unstuckme\u00a7f to unstuck",
         "\u00a7b/spawnme\u00a7f to get back to spawn (only work 3 times)"
+        "\u00a7b/sethome\u00a7f to set home location",
+        "\u00a7b/home\u00a7f to teleport to home (3 uses per server restart)",
     ]
 }
 TUTORIAL_TOPICS[TU_TOPICS.WEAPONS] = {
@@ -110,15 +112,10 @@ TUTORIAL_TOPICS[TU_TOPICS.SMUGGLING] = {
     title: "\u00a7e\u00a7l\ud83d\udc8a Sugar Smuggling",
     lines: [
 "\u00a7c\u26a0 \u00a7fHigh-risk, \u00a7ahigh-reward\u00a7f sugar runs",
-"\u00a7e\ud83c\udf69 \u00a7fFind sugarcane farms at these coords:",
-"\u00a7f|   \u00a7e2287 -53 1333",
-"\u00a7f|   \u00a7e2281 -53 1201",      
-"\u00a7f|   \u00a7e1957 -50 -192",            
-"\u00a7f|   \u00a7e950 -40 -277",
-"\u00a7f|   \u00a7e1168 -48 -4",       
-"\u00a7f|   \u00a7e2007 -51 115",            
-"\u00a7f|   \u00a7e1520 -41 -515",                  
-"\u00a7f|   \u00a7emore coords to be added..",          
+"\u00a7e\ud83c\udf69 \u00a7fThere are currently 3 levels of Sugar Farms you can find on the map:",
+"\u00a7e\ud83c\udf69 \u00a7fEasy",
+"\u00a7e\ud83c\udf69 \u00a7fMedium",
+"\u00a7e\ud83c\udf69 \u00a7fPro",
 "\u00a7c\u2694 \u00a7fTake out \u00a7cMercenaries\u00a7f guarding the farm",
 "\u00a7f  then add your name to their spawner safelist",
 "\u00a7b\ud83e\udd16 \u00a7fSpawn your own \u00a7bMercenaries\u00a7f to protect it",
