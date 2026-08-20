9f54682e2ca0162ec109784272db43040f30cffa
minecart551-prog <minecart551@gmail.com>
2026-08-20 14:40:40 +0700

    revert neonite

--- world/customnpcs/scripts/ecmascript/replicant/neonite.js
+++ world/customnpcs/scripts/ecmascript/replicant/neonite.js
@@ -7,12 +7,11 @@ function init(event){
      npc.getStats().getRanged().setStrength(3);
      npc.getStats().getRanged().setDelay(17, 17);
      npc.getStats().getRanged().setBurstDelay(1);
-     var coal = npc.world.createItem("coins:stone_coin", 12);
-     var beans = npc.world.createItem("minecraft:air", 1);
-     var milk = npc.world.createItem("minecraft:air", 1);
-     var mug = npc.world.createItem("minecraft:air", 1);
-     npc.getInventory().setDropItem(0, milk, 100);     
-     npc.getInventory().setDropItem(1, coal, 100);
+     var coal = npc.world.createItem("minecraft:coal", 1);
+     var beans = npc.world.createItem("minecraft:cocoa_beans", 1);
+     var milk = npc.world.createItem("farmersdelight:milk_bottle", 1);
+     var mug = npc.world.createItem("yuushya:mug", 1);
+     npc.getInventory().setDropItem(1, coal, 30);
      npc.getInventory().setDropItem(2, beans, 30);
      npc.getInventory().setDropItem(3, milk, 30);
      npc.getInventory().setDropItem(4, mug, 30);
